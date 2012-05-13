6bee97b7c955d1faa854e2077600e376ea594be4
Joe Germuska <joe@germuska.com>
2012-05-13 10:57:45 -0500

    redo kml

--- enlace/create_combined_kml.sh
+++ enlace/create_combined_kml.sh
@@ -2,7 +2,7 @@
 DATABASE='datakind'
 WORKING_DIR=`pwd`
 
-psql -c "drop table combined;"
+psql -c "drop table combined;" $DATABASE
 psql -c "create table combined (label varchar(200),type varchar(10))" $DATABASE
 psql -c "select AddGeometryColumn('combined', 'the_geom', 9102671, 'POINT', 2);" $DATABASE
 psql -c "create index combined_geom on combined using gist(the_geom)" $DATABASE
@@ -11,4 +11,5 @@ psql -c "insert into combined (type, label, the_geom) select 'crime', case_numbe
 
 psql -c "insert into combined (type, label, the_geom) select 'vacant', request_number || E'\\r\\n' || full_address, the_geom from vacant_311 where neigh_number = 110" $DATABASE
 
-ogr2ogr -f KML combined.kml -t_srs epsg:900913 "PG:dbname=${DATABASE}" combined
+# ogr2ogr -f KML combined.kml -t_srs epsg:900913 "PG:dbname=${DATABASE}" combined
+#ogr2ogr -f "ESRI Shapefile" combined -t_srs epsg:900913 "PG:dbname=${DATABASE}" combined
